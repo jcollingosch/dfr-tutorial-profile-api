172c6b98bd061b4c539afdde4b77517ce4936aab
Collin Gosch <collin.gosch@venmo.com>
2018-06-22 13:56:57 -0400

    added create, retrieve, update, partial_update, destroy to HelloViewSet

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -27,7 +27,6 @@ class HelloApiView(APIView):
 
 		return Response({'message': 'Hello!', 'an_apiview': an_apiview})
 
-
 	def post(self, request):
 		"""Create a hello message with our name"""
 
@@ -40,18 +39,15 @@ class HelloApiView(APIView):
 		else:
 			return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
 	def put(self, request, pk=None):
 		"""Handles updating an object."""
 
 		return Response({'method': 'put'})
 
-
 	def patch(self, request, pk=None):
 		"""Patch only updates fields provided in request"""
 
 		return Response({'method': 'patch'})
-
 
 	def delete(self, request, pk=None):
 		"""Delte an obj"""
@@ -61,6 +57,8 @@ class HelloApiView(APIView):
 
 class HelloViewSet(viewsets.ViewSet):
 	"""Test API ViewSet"""
+
+	serializer_class = serializers.HelloSerializer
 
 	def list(self, request):
 		"""Return a hello message"""
@@ -72,3 +70,35 @@ class HelloViewSet(viewsets.ViewSet):
 		]
 
 		return Response({'message': 'Hello!', 'viewset': a_viewset})
+
+	def create(self, request):
+		"""Create a new hello message"""
+
+		serializer = serializers.HelloSerializer(data=request.data)
+
+		if serializer.is_valid():
+			name = serializer.data.get('name')
+			message = 'Hello {0}'.format(name)
+			return Response({'message': message})
+		else:
+			return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+	def retrieve(self, request, pk=None):
+		"""Handles getting an object by its ID"""
+
+		return Response({'http_method': 'GET'})
+
+	def update(self, request, pk=None):
+		"""Handles updating an object by its ID"""
+
+		return Response({'http_method': 'PUT'})
+
+	def partial_update(self, request, pk=None):
+		"""Handles updating part of an object by its ID"""
+
+		return Response({'http_method': 'PATCH'})
+
+	def destroy(self, request, pk=None):
+		"""Handles removing an object by its ID"""
+
+		return Response({'http_method': 'DELETE'})
